22ace7f37eb756e4e893e43f493024db76a43476
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-08-15 08:40:44 +1000

    Business logic layer

--- springboot/src/main/java/com/example/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/example/springboot/SpringbootApplication.java
@@ -1,33 +1,11 @@
 package com.example.springboot;
 
-import com.example.springboot.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SpringbootApplication {
-	private static final LocalDate DOB = LocalDate.of(2000, Month.JANUARY, 5);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
-	}
-
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(
-				new Student(
-						1L,
-						"Rahul",
-						"rahul@gmail.com",
-						DOB,
-						LocalDate.now().getYear() - DOB.getYear()
-				)
-		);
 	}
 }
