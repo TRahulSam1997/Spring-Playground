edc2b82db2df890e49bd55a795bad212f83d3a3d
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-08-15 08:18:28 +1000

    Simple REST API created

--- springboot/src/main/java/com/example/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,13 +2,21 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
-//		System.out.println("Hello World");
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
+	@GetMapping
+	public List<String> hello() {
+		return List.of("Hello", "World.");
+	}
 }
