17582879d2a1ee7d3e355b53b2a43655f8ffaa2a
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-08-15 08:56:02 +1000

    Business logic layer

--- springboot/src/main/java/com/example/springboot/student/StudentController.java
+++ springboot/src/main/java/com/example/springboot/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.springboot.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +12,15 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/v1/students")
 public class StudentController {
-    private static final LocalDate DOB = LocalDate.of(2000, Month.JANUARY, 5);
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     @GetMapping
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Rahul",
-                        "rahul@gmail.com",
-                        DOB,
-                        LocalDate.now().getYear() - DOB.getYear()
-                )
-        );
+        return studentService.getStudents();
     }
 }
