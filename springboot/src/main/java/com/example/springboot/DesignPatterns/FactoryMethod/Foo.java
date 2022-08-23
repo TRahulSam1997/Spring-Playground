d40a8039cf397940b2fe629292dab538e30569e1
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-08-23 12:48:32 +1000

    factory method design pattern

---
+++ springboot/src/main/java/com/example/springboot/DesignPatterns/FactoryMethod/Foo.java
@@ -0,0 +1,7 @@
+package com.example.springboot.DesignPatterns.FactoryMethod;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Foo {
+}
--- springboot/src/main/java/com/example/springboot/SpringbootApplication.java
+++ springboot/src/main/java/com/example/springboot/SpringbootApplication.java
@@ -1,12 +1,29 @@
 package com.example.springboot;
 
+import com.example.springboot.DesignPatterns.FactoryMethod.Application;
+import com.example.springboot.DesignPatterns.FactoryMethod.ApplicationConfig;
+import com.example.springboot.DesignPatterns.FactoryMethod.Bar;
+import com.example.springboot.DesignPatterns.FactoryMethod.Foo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class SpringbootApplication {
+
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 
-		SpringApplication.run(SpringbootApplication.class, args);
+		ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+
+		Foo foo = context.getBean(Foo.class);
+		Bar bar = context.getBean(Bar.class, "className");
+
+		System.out.println("foo -> " + foo);
+		System.out.println("Bar's name " + bar.getName());
+
+
+//		SpringApplication.run(SpringbootApplication.class, args);
 	}
 }
