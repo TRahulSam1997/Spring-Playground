2c6c400b550c645948a8e5855c83055b1679d95a
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-09-08 21:14:42 +1000

    controller CRUD operations added

--- streams-schedule/src/main/java/com/spring/rest/streamsschedule/controller/LiveStreamController.java
+++ streams-schedule/src/main/java/com/spring/rest/streamsschedule/controller/LiveStreamController.java
@@ -1,19 +1,51 @@
 package com.spring.rest.streamsschedule.controller;
 
+import com.spring.rest.streamsschedule.exception.LiveStreamNotFoundException;
 import com.spring.rest.streamsschedule.model.LiveStream;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.spring.rest.streamsschedule.repository.LiveStreamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/streams")
 public class LiveStreamController {
 
-    @GetMapping
-    public List<LiveStream> findAll() {
-        return null;
+    private final LiveStreamRepository repository;
+
+    @Autowired
+    public LiveStreamController(LiveStreamRepository repository) {
+        this.repository = repository;
     }
 
+    @GetMapping
+    public List<LiveStream> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public LiveStream findById(@PathVariable String id) throws LiveStreamNotFoundException {
+        return repository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public LiveStream create(@Valid @RequestBody LiveStream stream) {
+        return repository.create(stream);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody LiveStream stream, @PathVariable String id) {
+        repository.update(stream, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        repository.delete(id);
+    }
 }
---
+++ streams-schedule/src/main/java/com/spring/rest/streamsschedule/exception/LiveStreamNotFoundException.java
@@ -0,0 +1,8 @@
+package com.spring.rest.streamsschedule.exception;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus
+public class LiveStreamNotFoundException extends Exception {
+
+}
